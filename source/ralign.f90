656e7d562a184338a0d6fa4cc07515200a6424df
cronofugo <manuelvsqz@gmail.com>
2022-06-23 03:25:37 -0500

    Rename program

--- source/ralign.f90
+++ source/ralign.f90
@@ -1,106 +1,157 @@
-program ralign
+subroutine align(natom, atoms0, atoms1, label0, label1, mapcount, atomaplist, rotmatlist)
+! Purpose: Superimpose coordinates of atom sets atoms0 and atoms1
+
+use iso_fortran_env, only: output_unit
+use iso_fortran_env, only: error_unit
 
 use options
-use messages
-use decoding
-use readwrite
-use utilities
+use random
+use sorting
 use rotation
+use translation
+use alignment
+use remapping
+use assortment
+use chemistry
+use messages
 
 implicit none
 
-integer natom, mapcount
-integer, dimension(:, :), allocatable :: atomaplist
-real, dimension(:, :, :), allocatable :: rotmatlist
-integer, dimension(:, :), allocatable :: bonds0, bonds1
-real, dimension(:, :), allocatable :: atoms0, atoms1
-character(32), dimension(:), allocatable :: label0, label1
-character(512) title0, title1
+integer, intent(in) :: natom
+integer, intent(out) :: mapcount
+real, dimension(3, 3, maxcount), intent(out) :: rotmatlist
+integer, dimension(natom, maxcount), intent(out) :: atomaplist
+character(32), dimension(natom), intent(inout) :: label0, label1
+real, dimension(3, natom), intent(inout) :: atoms0, atoms1
 
 integer i
-character(32) arg
+integer nblock0, nblock1
+integer, dimension(:), allocatable :: seed
+integer, dimension(:), allocatable :: znum0, znum1
+integer, dimension(:), allocatable :: blocktype0, blocktype1
+integer, dimension(:), allocatable :: blocksize0, blocksize1
+integer, dimension(:), allocatable :: order0, reorder0, order1
+real, dimension(:), allocatable :: weights
+real, dimension(:, :), allocatable :: bias
+real, dimension(nelem) :: atomweight
 
-! Set default options 
-
-biased = .false.
-iterative = .false.
-testing = .false.
-live = .false.
-
-maxcoord = 0
-maxcount = -1
-maxrecord = 9
-
-scaling = 1000.
-tolerance = 0.1
-
-weighting = 'none'
-output_format = 'xyz'
-
-converge = .false.
-
-! Get user options 
-
-call initarg()
-
-do while (getarg(arg))
-
-    select case (arg)
-    case ('-iter')
-        iterative = .true.
-    case ('-converge')
-        converge = .true.
-    case ('-bias')
-        biased = .true.
-    case ('-testing')
-        testing = .true.
-    case ('-live')
-        live = .true.
-    case ('-scale')
-        call readoptarg(arg, scaling)
-    case ('-tol')
-        call readoptarg(arg, tolerance)
-    case ('-weight')
-        call readoptarg(arg, weighting)
-    case ('-out')
-        call readoptarg(arg, output_format)
-    case ('-n')
-        call readoptarg(arg, maxrecord)
-    case default
-        call readarg(arg, maxcount)
-    end select
-
-end do
-
-! Read coordinates
-
-call readmol('xyz', title0, label0, atoms0, bonds0)
-call readmol('xyz', title1, label1, atoms1, bonds1)
-
-! Check number of atoms
-
-if (size(label0) == size(label1)) then
-    natom = size(label0)
-else
-    call error('The molecules do not have the same number of atoms!')
+if (maxcount < 0) then
+    call error('Argument is missing!')
 end if
 
-! Allocate records
+if (converge) then
+    stop_test => match_stop_test
+else
+    stop_test => trial_stop_test
+end if
 
-allocate(rotmatlist(3, 3, maxrecord))
-allocate(atomaplist(natom, maxrecord))
+select case (weighting)
+case ('none')
+    atomweight = [(1., i=1, nelem)]
+case ('mass')
+    atomweight = stdmatom
+case default
+    call error('Invalid weighting option: '//trim(weighting))
+end select
 
-! Align atoms
+! Allocate arrays
 
-call superpose(natom, atoms0, atoms1, label0, label1, mapcount, atomaplist, rotmatlist)
+allocate(weights(natom))
+allocate(bias(natom, natom))
+allocate(order0(natom), order1(natom), reorder0(natom))
+allocate(blocktype0(natom), blocktype1(natom))
+allocate(blocksize0(natom), blocksize1(natom))
+allocate(znum0(natom), znum1(natom))
 
-! Write aligned coordinates
+! Get atomic numbers
 
-do i = 1, mapcount
-    open (file_unit, file='aligned_'//str(i)//'.'//trim(output_format), action='write', status='replace')
-    call writemol(file_unit, [(i, i=1, natom)], title0, label0, atoms0, bonds0)
-    call writemol(file_unit, atomaplist(:, i), title1, label1, rotated(natom, rotmatlist(:, :, i), atoms1), bonds1)
-    close (file_unit)
+do i = 1, natom
+    label0(i) = upper(label0(i))
+    label1(i) = upper(label1(i))
+    znum0(i) = znum(label0(i))
+    znum1(i) = znum(label1(i))
 end do
 
-end program
+! Calculate normalized weights
+
+weights = atomweight(znum0)/sum(atomweight(znum0))
+
+! Group atoms by label
+
+call grouplabels(natom, label0, nblock0, blocksize0, blocktype0)
+call grouplabels(natom, label1, nblock1, blocksize1, blocktype1)
+
+! Sort equivalent atoms in contiguous blocks
+
+order0 = sortorder(blocktype0, natom)
+order1 = sortorder(blocktype1, natom)
+reorder0 = inversemap(order0)
+
+label0 = label0(order0)
+label1 = label1(order1)
+
+atoms0 = atoms0(:, order0)
+atoms1 = atoms1(:, order1)
+
+znum0 = znum0(order0)
+znum1 = znum1(order1)
+
+weights = weights(order0)
+
+! Abort if there are incompatible atomic symbols
+
+if (any(label0 /= label1)) then
+    call error('The molecules are not isomers!')
+end if
+
+! Abort if there are incompatible atomic labels
+
+if (any(label0 /= label1)) then
+    call error('There are incompatible atomic labels!')
+end if
+
+! Translate molecules to their centroid
+
+call translate(natom, centroid(natom, weights, atoms0), atoms0)
+call translate(natom, centroid(natom, weights, atoms1), atoms1)
+
+! Set bias for non equivalent atoms 
+
+call setadjbias(natom, nblock0, blocksize0, atoms0, atoms1, bias)
+
+! Initialize random number generator
+
+call init_random_seed(seed)
+call random_seed(put=seed)
+
+! Remap atoms to minimize distance and difference
+
+if (maxcount >= 1) then
+    call remapatoms(natom, nblock0, blocksize0, atoms0, atoms1, weights, mapcount, atomaplist, bias)
+else
+    mapcount = 1
+    atomaplist(:, 1) = [(i, i=1, natom)]
+    call print_header()
+    call print_stats(0, 0, 0, 0., 0., 0., leastsquaredist(natom, weights, atoms0, atoms1, atomaplist(:, 1)))
+    call print_footer(.false., .false., 0, 0)
+end if
+
+! Calculate rotation quaternions
+
+do i = 1, mapcount
+    rotmatlist(:, :, i) = quat2mat(leastrotquat(natom, weights, atoms0, atoms1, atomaplist(:, i)))
+end do
+
+! Restore original atom order
+
+label0 = label0(reorder0)
+label1 = label1(reorder0)
+
+atoms0 = atoms0(:, reorder0)
+atoms1 = atoms1(:, reorder0)
+
+do i = 1, mapcount
+    atomaplist(:, i) = atomaplist(reorder0, i)
+end do
+
+end subroutine
