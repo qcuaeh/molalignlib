3008ec09724ac37b195f32f1e678cbfa0ada647f
cronofugo <manuelvsqz@gmail.com>
2022-06-23 19:39:20 -0500

    Add comments

--- fortran/maputils.f90
+++ fortran/maputils.f90
@@ -5,17 +5,17 @@ implicit none
 contains
 
 function inversemap(mapping)
+! Get the inverse mapping of mapping
     integer, intent(in), dimension(:) :: mapping
     integer, dimension(size(mapping)) :: inversemap
-
     integer i
-
     do i = 1, size(mapping)
         inversemap(mapping(i)) = i
     end do
 end function
 
 logical function propermap(mapping, n)
+! Check if mapping is a proper mapping
     integer, intent(in) :: n, mapping(:)
     integer i
     propermap = .false.
