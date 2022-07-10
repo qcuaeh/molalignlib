a386133cef15b93a593e8af4dd120aeea7c59b8c
cronofugo <manuelvsqz@gmail.com>
2022-07-10 01:12:21 -0500

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,57 +1,57 @@
 # Compile source file
 compile () {
    sourcefile=$SRCDIR/$1
-   buildfile=$OBJDIR/$1
-   objectfile=$OBJDIR/${1%.*}.o
+   buildfile=$BUILDIR/$1
+   objectfile=$BUILDIR/${1%.*}.o
    objectlist+=("$objectfile")
    if ! test -e "$objectfile" || ! test -e "$buildfile" || ! diff -q "$sourcefile" "$buildfile" > /dev/null; then
       rm -f "$objectfile"
       cp -p "$sourcefile" "$buildfile"
       echo Compiling "$1"
-      $FORTRAN $libflags $optflags -c "$sourcefile" -o "$objectfile" -I "$OBJDIR" -J "$OBJDIR" || exit
+      $FORTRAN $libflags $optflags -c "$sourcefile" -o "$objectfile" -I "$BUILDIR" -J "$BUILDIR" || exit
    fi
 }
 
 shopt -s nullglob
 
-options=$(getopt -a -o '' -l exe,lib,fast,debug -- "$@")
+options=$(getopt -a -o '' -l slow,fast,debug,recom -- "$@")
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "$options"
 
 optlevel=fast
-buildtype=program
 
 while true; do
    case "$1" in
-      --prog) buildtype=program; shift;;
-      --lib) buildtype=library; shift;;
+      --slow) optlevel=slow; shift;;
       --fast) optlevel=fast; shift;;
       --debug) optlevel=debug; shift;;
+      --recom) recompile=true; shift;;
       --) shift; break ;;
    esac
 done
 
-case $buildtype in
+case $BUILDTYPE in
    program) libflags='';;
    library) libflags='-fPIC';;
-   *) echo Invalid build type: $buildtype; exit;;
+   *) echo Invalid build type: $BUILDTYPE; exit;;
 esac
 
 case $optlevel in
+   slow) optflags='-O0';;
    fast) optflags='-O3 -ffast-math';;
-   debug) optflags='-g -fbounds-check -fbacktrace -ffpe-trap=zero,invalid,overflow -O0 -Wall';;
+   debug) optflags='-O0 -g -fbounds-check -fbacktrace -Wall -ffpe-trap=zero,invalid,overflow';;
    *) echo Invalid optimization level: $optlevel; exit;;
 esac
 
 NAME=ralign
-ROOTDIR=$(cd -- "$(dirname "$0")" && pwd)
-SRCDIR=$ROOTDIR/source
-BINDIR=$ROOTDIR/bin
-BUILDIR=$ROOTDIR/_build_dir
-OBJDIR=$BUILDIR/$buildtype/$optlevel
+HERE=$(cd -- "$(dirname "$0")" && pwd)
+SRCDIR=$HERE/source
+BINDIR=$HERE/bin
+BUILDROOT=$HERE/_build_dir
+BUILDIR=$BUILDROOT/$BUILDTYPE/$optlevel
 
 if [[ -d $BINDIR ]]; then
-   case $buildtype in
+   case $BUILDTYPE in
       program) rm -f "$BINDIR"/"$NAME";;
       library) rm -f "$BINDIR"/"$NAME".so "$BINDIR"/"$NAME".*.so;;
    esac
@@ -59,12 +59,12 @@ else
    mkdir "$BINDIR"
 fi
 
-if [[ -d $OBJDIR ]]; then
-  if $RECOMPILE; then
-     rm -f "$OBJDIR"/*
+if [[ -d $BUILDIR ]]; then
+  if $recompile; then
+     rm -f "$BUILDIR"/*
   fi
 else
-  mkdir -p "$OBJDIR"
+  mkdir -p "$BUILDIR"
 fi
 
 objectlist=()
@@ -74,11 +74,11 @@ while IFS= read -r line; do
   eval set -- "$line"
   compile "$1"
   if [[ -n $2 ]]; then
-     exportlist+=("$OBJDIR"/"$1")
+     exportlist+=("$BUILDIR"/"$1")
   fi
 done < <(grep -v '^#' "$SRCDIR"/compilelist)
 
-case $buildtype in
+case $BUILDTYPE in
 program)
    echo Linking program...
    $FORTRAN -L "$LAPACK" -llapack -o "$BINDIR"/"$NAME" "${objectlist[@]}"
@@ -88,7 +88,7 @@ library)
 #   $FORTRAN -shared -L "$LAPACK" -llapack -o "$BINDIR"/"$NAME".so "${objectlist[@]}"
    echo Linking python library...
    pushd "$BINDIR"
-   $F2PY -h "$OBJDIR"/"$NAME".pyf --overwrite-signature -m "$NAME" "${exportlist[@]}" --quiet
-   $F2PY -c "$OBJDIR"/"$NAME".pyf -I"$OBJDIR" -L"$LAPACK" -llapack "${objectlist[@]}" --quiet
+   $F2PY -h "$BUILDIR"/"$NAME".pyf --overwrite-signature -m "$NAME" "${exportlist[@]}" --quiet
+   $F2PY -c "$BUILDIR"/"$NAME".pyf -I"$BUILDIR" -L"$LAPACK" -llapack "${objectlist[@]}" --quiet
    popd
 esac
