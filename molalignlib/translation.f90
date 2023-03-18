d70438c84fdc2f8f175b4af0c051f4bc0af0ee63
cronofugo <manuelvsqz@gmail.com>
2023-03-18 01:06:56 -0600

    Simplify code

--- molalignlib/translation.f90
+++ molalignlib/translation.f90
@@ -21,7 +21,6 @@ implicit none
 
 private
 public centroid
-public centered
 public translate
 public translated
 
@@ -71,18 +70,5 @@ subroutine translate(natom, coords, vector)
    end do
 
 end subroutine
-
-function centered(natom, coords, vector)
-! Purpose: Translate atomic coordinates to its vector of geometry
-   integer, intent(in) :: natom
-   real(wp), intent(in) :: coords(3, natom), vector(3)
-   real(wp) :: centered(3, natom)
-   integer :: i
-
-   do i = 1, natom
-      centered(:, i) = coords(:, i) - vector(:)
-   end do
-
-end function
 
 end module
