0a2a4f25ba27471788e139c9a0da94e85450a891
josemvas <21151851+josemvas@users.noreply.github.com>
2026-01-24 21:45:31 +0000

    Delete obsolete modules and programs

--- programs/atormsd.f90
+++ programs/atormsd.f90
@@ -29,7 +29,6 @@ use file_reading
 use file_writing
 use argparse
 use assorting
-use biasing_isomer
 use pruning_atoms
 use recording
 use alignment_atoms
--- programs/conformsd.f90
+++ programs/conformsd.f90
@@ -28,7 +28,6 @@ use file_reading
 use file_writing
 use argparse
 use assorting
-use biasing_isomer
 use pruning_atoms
 use recording
 use assignment_conformer
